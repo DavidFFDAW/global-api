522913753ffab63f54807b029b996438455e595d
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-04-24 10:06:14 +0200

    feat: wip teams API routes

--- models/2kmanager/team.model.js
+++ models/2kmanager/team.model.js
@@ -27,10 +27,7 @@ class Team extends Connection {
         return this.query(sql);
     }
 
-    async getAllTeamsWithMembers() { 
-        const teams = await this.getTeams();
-        const teamMembers = await this.getTeamMembers();
-        
+    assignTeamMembers(teams, teamMembers) { 
         return teams.map((team) => {
             const members = teamMembers.filter((member) => member.team_id === team.id);
             return {
@@ -38,6 +35,50 @@ class Team extends Connection {
                 members,
             };
         });
+    }
+
+    async getAllTeamsWithMembers() { 
+        const teams = await this.getTeams();
+        const teamMembers = await this.getTeamMembers();
+
+        return this.assignTeamMembers(teams, teamMembers);
+    }
+
+    getSingleTeam(params) { 
+        const sql = `SELECT t.id AS id, t.name AS \`name\`, t.average AS average, w1.name AS member_champion_1, w1.image_name AS member_champion_1_image, w2.name AS member_champion_2, w2.image_name AS member_champion_2_image FROM teams t LEFT JOIN wrestler w1 ON t.member_champion_1 = w1.id LEFT JOIN wrestler w2 ON t.member_champion_2 = w2.id WHERE t.id = ${params.id}`;
+        return this.query(sql);
+
+    }
+
+    getSingleTeamWithMembers(params) { 
+        const sql = 'SELECT wt.*, w.name, w.image_name FROM wrestler_team wt INNER JOIN wrestler w ON w.id = wt.wrestler_id WHERE wt.team_id = ' + params.id;
+        return this.query(sql);
+    }
+
+    async getSingleTeamWithMembersById(params) {
+        const team = await this.getSingleTeam(params);
+        const teamMembers = await this.getSingleTeamWithMembers(params);
+
+        return this.assignTeamMembers(team, teamMembers);
+    }
+
+    createParams = ['name', 'average', 'members'];
+    createTeam(body) {
+        if (!body || Object.keys(body).length === 0) { 
+            throw new Error('No body provided');
+        }
+
+        const missingEntries = Object.entries(body).filter(
+            ([key, _]) => {
+                return this.createParams.includes(key);
+            }
+        );
+
+        if (missingEntries.length > 0) {
+            throw new Error('Missing entries: ' + missingEntries.join(', '));
+        }
+
+        return missingEntries;
     }
 }
 
--- routes/2kmanager/teams.routes.js
+++ routes/2kmanager/teams.routes.js
@@ -4,80 +4,92 @@ const { Team } = require("../../models/2kmanager/team.model");
 
 const team = new Team();
 
-router.get("/", async function (req, res, next) {
+router.get("/", async function (_, res, next) {
+
+    try {
+        const list = await team.getTeams();
+        res.status(200).json(list);
+    } catch (err) {
+        res.status(err.statusCode || 500).json({
+            type: "Error while getting teams",
+            message: err.message,
+        });
+    }
+});
+
+router.get("/members", async function (_, res, next) {
 
     try {
         const teams = await team.getAllTeamsWithMembers();
         res.status(200).json(teams);
     } catch (err) {
         res.status(err.statusCode || 500).json({
-            type: "Error while getting all wrestlers",
+            type: "Error while getting all teams with members",
             message: err.message,
         });
     }
 });
 
-router.get("/released", async function (req, res, next) {
-    const queryParameters = req.query;
-    queryParameters.status = "released";
 
-    try {
-        const wrestlers = await (queryParameters
-            ? wrestler.findByFilter(queryParameters)
-            : wrestler.findAll());
-        res.status(200).json(wrestlers);
-    } catch (err) {
-        res.status(err.statusCode || 500).json({
-            type: "Error while getting released wrestlers",
-            message: err.message,
-        });
-    }
-});
-
-router.get("/active", async function (req, res, next) {
-    const queryParameters = req.query;
-    queryParameters.status = "active";
-
-    try {
-        const wrestlers = await (queryParameters
-            ? wrestler.findByFilter(queryParameters)
-            : wrestler.findAll());
-        res.status(200).json(wrestlers);
-    } catch (err) {
-        res.status(err.statusCode || 500).json({
-            type: "Error while getting active wrestlers",
-            message: err.message,
-        });
-    }
-});
-
-router.get("/single/:id", async function (req, res, next) {
+router.get("/team/:id", async function (req, res, next) {
     const queryParameters = req.query;
     queryParameters.id = req.params.id;
 
     try {
-        const singleWrestlerByID = await wrestler.findOneByFilter(
+        const singleTeam = await team.getSingleTeam(
             queryParameters
         );
-        res.status(200).json(singleWrestlerByID);
+        res.status(200).json(singleTeam);
     } catch (err) {
         res.status(err.statusCode || 500).json({
-            type: "Error while getting active wrestlers",
+            type: "Error while getting single team by id: " + queryParameters.id,
             message: err.message,
         });
     }
 });
 
-router.post("/upsert", async function (req, res, next) {
+router.get("/team/members/:id", async function (req, res, next) {
+    const queryParameters = req.query;
+    queryParameters.id = req.params.id;
+
+    try {
+        const singleTeamWithMembers = await team.getSingleTeamWithMembersById(
+            queryParameters
+        );
+        res.status(200).json(singleTeamWithMembers);
+    } catch (err) {
+        res.status(err.statusCode || 500).json({
+            type: "Error while getting single team with members by id: " + queryParameters.id,
+            message: err.message,
+        });
+    }
+});
+
+router.post("/create/team", async function (req, res, next) {
     const isValidToken = await validateToken(req.headers);
     if (!isValidToken) return res.status(403).json({ message: "Unauthorized" });
 
     try {
-        const upserted = await wrestler.upsert(req.body);
-        return res.status(200).json(upserted);
+        const created = await team.createTeam(req.body);
+        return res.status(200).json(created);
     } catch (err) {
         return res.status(err.statusCode || 500).json({
-            type: "Error while upserting wrestler",
+            type: "Error while creating team",
+            message: err.message,
+        });
+    }
+});
+
+router.put("/update/team", async function (req, res, next) {
+    const isValidToken = await validateToken(req.headers);
+    if (!isValidToken) return res.status(403).json({ message: "Unauthorized" });
+
+    try {
+        const created = await team.createTeam(req.body);
+        return res.status(200).json(created);
+    } catch (err) {
+        return res.status(err.statusCode || 500).json({
+            type: "Error while updating team",
             message: err.message,
         });
     }
